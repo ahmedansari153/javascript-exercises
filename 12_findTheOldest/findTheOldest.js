f9386942180731646c49851717735195a9eb1092
ahmedansari153 <16545540+ahmedansari153@users.noreply.github.com>
2023-09-29 15:10:49 -0500

    Finished exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = people.map(person => (person.yearOfDeath !=null ? person.yearOfDeath:new Date().getFullYear()) - (person.yearOfBirth));
+    return people[ages.indexOf(Math.max(...ages))] ;
 };
 
 // Do not edit below this line
